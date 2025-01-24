c2c4162932467053f1d4a88e499753e574f498d5
mmkvdev <madhuv299@gmail.com>
2025-01-24 10:49:20 +0530

    pattern/sliding_window: refactoring max sub array sum optimal solution

--- patterns/sliding-window/problems/refactored/maxSubArraySum.js
+++ patterns/sliding-window/problems/refactored/maxSubArraySum.js
@@ -24,8 +24,39 @@ const maxSubArraySum = (arr, num) => {
   return maxSum;
 };
 
+const maxSubArraySum2 = (arr, num) => {
+  if (arr.length < num) {
+    return null;
+  }
+  let l = 0,
+    r = num - 1,
+    maxSum = 0,
+    tempSum = 0;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+  while (r < arr.length - 1) {
+    tempSum -= arr[l];
+    l++;
+    r++;
+    tempSum += arr[r];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+};
+
 console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
 console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
 console.log(maxSubArraySum([4, 2, 1, 6], 1)); // 6
 console.log(maxSubArraySum([4, 2, 1, 6, 2], 4)); // 13
 console.log(maxSubArraySum([], 4)); // null
+
+console.log(maxSubArraySum2([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubArraySum2([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubArraySum2([4, 2, 1, 6], 1)); // 6
+console.log(maxSubArraySum2([4, 2, 1, 6, 2], 4)); // 13
+console.log(maxSubArraySum2([], 4)); // null
