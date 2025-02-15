a7390d12d3c74cd29f3fab7bee0b03d5ed18abc5
mmkvdev <madhuv299@gmail.com>
2025-02-15 17:29:46 +0530

    problems: isPalindrome

---
+++ problems/isPalindrome.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * Write a recursive function called isPalindrome
+ * which returns true if the string passed to it is a palindrome
+ * (reads the same forward and backward).
+ * Otherwise it returns false.
+ */
+
+const isPalindrome = (str) => {
+  if (str.length === 1) return true;
+  if (str.length === 2) {
+    return str[0] === str[1];
+  }
+  if (str[0] === str.slice(-1)) {
+    return isPalindrome(str.slice(1, -1));
+  }
+
+  return false;
+};
+
+// anna
+// mom
+
+console.log(isPalindrome("abc"));
+console.log(isPalindrome("anna"));
+console.log(isPalindrome("mom"));
