3a506c0a1fc0f62ad7e361b90683bbc766958f88
mmkvdev <madhuv299@gmail.com>
2025-02-21 22:42:20 +0530

    patterns/queues: array implementation

---
+++ patterns/queues/arrays/index.js
@@ -0,0 +1,19 @@
+// Implemeting Queues using Arrays
+
+// Insertions and Removal
+
+// push() and shift() for enqueue and dequeue operations
+
+let a = [];
+a.push("first");
+a.push("second");
+a.push("third");
+a.shift();
+
+// unshift() and pop() for enqueue and dequeue operations
+
+let b = [];
+b.unshift("first");
+b.unshift("second");
+b.unshift("third");
+b.pop();
