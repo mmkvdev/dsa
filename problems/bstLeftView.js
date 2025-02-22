bffcdef0a513f2b2088016372eca0c53188d7229
Madhu Kiran Varma <madhuv299@gmail.com>
2025-02-22 20:50:52 +0530

    problems: BST Left View

---
+++ problems/bstLeftView.js
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function buildTree(str) {
+    // Corner Case
+    if (str.length === 0 || str[0] === "N") return null;
+
+    // Create the root of the tree
+    let root = new Node(parseInt(str[0]));
+
+    // Push the root to the queue
+    let queue = [];
+    let start = 0;
+    queue.push(root);
+
+    // Starting from the second element
+    let i = 1;
+    while (queue.length !== start && i < str.length) {
+
+        // Get and remove the front of the queue
+        let currNode = queue[start];
+        start++;
+
+        // Get the current node's value from the string
+        let currVal = str[i];
+
+        // If the left child is not null
+        if (currVal !== "N") {
+
+            // Create the left child for the current node
+            currNode.left = new Node(parseInt(currVal));
+
+            // Push it to the queue
+            queue.push(currNode.left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= str.length) break;
+        currVal = str[i];
+
+        // If the right child is not null
+        if (currVal !== "N") {
+
+            // Create the right child for the current node
+            currNode.right = new Node(parseInt(currVal));
+
+            // Push it to the queue
+            queue.push(currNode.right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let input_ar1 = readLine().split(' ');
+        let root = buildTree(input_ar1);
+        let obj = new Solution();
+        let res = obj.leftView(root);
+        let s = '';
+        if (res.length === 0) {
+            s += "[]";
+        }
+        for (let i = 0; i < res.length; i++) {
+            if (!isNaN(res[i])) s += res[i] + " ";
+        }
+        console.log(s);
+
+        console.log("~");
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {Node} root
+ * @returns {number[]}
+ */
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+class Solution {
+    leftView(root) {
+        if (!root) return [];
+        // your code here
+        let queue = [root], visited = [];
+        while (queue.length) {
+            let queueSize = queue.length;
+            let level = [];
+            
+            for (let i = 0; i < queueSize; i++) {
+                let current = queue.shift();
+                if (i === 0) level.push(current.data);
+                if (current && current.left) queue.push(current.left);
+                if (current && current.right) queue.push(current.right);
+            }
+            visited.push(level);
+        }
+        
+        return visited;
+    }
+}
