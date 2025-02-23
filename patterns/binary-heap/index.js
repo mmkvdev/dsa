f7f73ccd4eeb00887a8815a7ee2bdf9678086e0f
mmkvdev <madhuv299@gmail.com>
2025-02-23 09:03:42 +0530

    patterns/binary-heap: binary heap bubbledown implementation

--- patterns/binary-heap/index.js
+++ patterns/binary-heap/index.js
@@ -21,6 +21,54 @@ class MaxBinaryHeap {
       idx = parentIndex;
     }
   }
+
+  extractMax() {
+    let maxElement = this.values[0];
+    let endElement = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = endElement;
+      // bubble down
+      this.bubbleDown();
+    }
+    return maxElement;
+  }
+
+  bubbleDown() {
+    let idx = 0;
+    const element = this.values[0];
+    const length = this.values.length;
+
+    while (true) {
+      let leftChildIndex = 2 * idx + 1;
+      let rightChildIndex = 2 * idx + 2;
+      let leftChild,
+        rightChild,
+        swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 const heap = new MaxBinaryHeap();
