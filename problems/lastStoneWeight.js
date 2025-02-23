5037ee445636692fc03b57f053fb40b6137b7a1c
Madhu Kiran Varma <madhuv299@gmail.com>
2025-02-23 13:20:45 +0530

    problems: optimised last stone weight
    added custom well written max priority queue implementation

--- problems/lastStoneWeight.js
+++ problems/lastStoneWeight.js
@@ -1,14 +1,89 @@
 /**
  * @param {number[]} stones
  * @return {number}
- * https://leetcode.com/problems/last-stone-weight/description/
+ * https://leetcode.com/problems/last-stone-weight/
  */
-var lastStoneWeight = function(stones) {
-    while (stones.length > 1) {
-        stones.sort((a, b) => b - a);
-        stones[1] = stones[0] - stones[1];
-        stones.shift();
+
+class MaximumPriorityQueue {
+    constructor () {
+        this.values = [];
     }
 
-    return stones[0];
+    enqueue (val) {
+        this.values.push(val);
+        this.bubbleUp();
+    }
+
+    bubbleUp () {
+        let idx = this.values.length - 1;
+        let childElement = this.values[idx];
+
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parentElement = this.values[parentIdx];
+
+            if (parentElement >= childElement) break;
+            this.values[parentIdx] = childElement;
+            this.values[idx] = parentElement;
+            idx = parentIdx;
+        }
+    }
+
+    dequeue () {
+        const maxElement = this.values[0];
+        const endElement = this.values.pop();
+
+        if (this.values.length > 0) {
+            this.values[0] = endElement;
+            this.bubbleDown();
+        }
+        return maxElement;
+    }
+
+    bubbleDown () {
+        let idx = 0;
+        let parentElement = this.values[idx];
+        let length = this.values.length;
+
+        while (true) {
+            let leftChildIndex = 2 * idx + 1;
+            let rightChildIndex = 2 * idx + 2;
+            let leftChild, rightChild, swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                if (leftChild > parentElement) {
+                    swap = leftChildIndex;
+                }
+            }
+
+            if (rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex];
+                if ((swap === null && rightChild > parentElement) || (swap != null && rightChild > leftChild)) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = parentElement;
+            idx = swap;
+        }
+    }
+}
+
+var lastStoneWeight = function(stones) {
+    const heap = new MaximumPriorityQueue();
+
+    // keep enqueueing all the stones to the heap
+    stones.forEach(stone => heap.enqueue(stone));
+
+    // console.log(heap, heap.values.length);
+    while (heap.values.length > 1) {
+        // dequeue the top two heaviest stones and combine them and re-enqueue the result to the heap
+        heap.enqueue(heap.dequeue() - heap.dequeue());
+    }
+
+    // return the largest element in the heap i.e., the first element
+    return heap.values[0];
 };
