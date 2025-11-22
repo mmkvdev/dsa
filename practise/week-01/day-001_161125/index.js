81d5eb4d142be35209b26ab3eaf13fdba6cedae6
mmkvdev <madhuv299@gmail.com>
2025-11-22 09:45:52 +0530

    refactor: more solutions including optimised version for character count of a string

--- practise/week-01/day-001_161125/index.js
+++ practise/week-01/day-001_161125/index.js
@@ -1,9 +1,11 @@
 // write a function which takes in a string and returns counts of each character in the string
 
+// approach 1
 const isAlphaNumeric = (char) => {
   return (char >= "a" && char <= "z") || (char >= "0" && char <= "9");
 };
 
+// solution 1
 const charCount = (str) => {
   let result = {};
 
@@ -17,6 +19,8 @@ const charCount = (str) => {
   return result;
 };
 
+// logs for solution 1
+console.log("Solution 1 logs: ");
 console.log(charCount("aaaa"));
 /**
  * { a: 4 }
@@ -28,6 +32,135 @@ console.log(charCount("hello"));
  */
 
 console.log(charCount("Your PIN number is 1234!"));
+/**
+ * { 1: 1, 2: 1, 3: 1, 4: 1, b: 1, e: 1, i: 2, m: 1, n: 2, o: 1, p: 1, r: 2, s: 1, u: 2, y: 1 }
+ */
+
+// -----------------[SOLUTION: 2]--------------------------- //
+
+// approach 2
+const isAlphaNumericUsingRegex = (char) => {
+  return /[a-z0-9]/.test(char);
+};
+
+// solution 2
+const charCountUsingRegex = (str) => {
+  let result = {};
+
+  for (let char of str) {
+    let currentChar = char.toLowerCase();
+    if (isAlphaNumericUsingRegex(currentChar)) {
+      result[currentChar] = result[currentChar] ? ++result[currentChar] : 1;
+    }
+  }
+
+  return result;
+};
+
+// logs for solution 2
+console.log("Solution 2 logs: ");
+console.log(charCountUsingRegex("aaaa"));
+/**
+ * { a: 4 }
+ */
+
+console.log(charCountUsingRegex("hello"));
+/**
+ * { h: 1, e: 1, l: 2, o: 1 }
+ */
+
+console.log(charCountUsingRegex("Your PIN number is 1234!"));
+/**
+ * { 1: 1, 2: 1, 3: 1, 4: 1, b: 1, e: 1, i: 2, m: 1, n: 2, o: 1, p: 1, r: 2, s: 1, u: 2, y: 1 }
+ */
+
+// -----------------[SOLUTION: 3]--------------------------- //
+
+// approach 3
+const isAlphaNumericUsingCharCode = (char) => {
+  const code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper alpha (A-Z)
+    !(code > 96 && code < 123) // lower alpha (a-z)
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+// solution 3
+const charCountUsingCharCode = (str) => {
+  let obj = {};
+  for (let char of str) {
+    if (isAlphaNumericUsingCharCode(char)) {
+      let currentChar = char.toLowerCase();
+      obj[currentChar] = ++obj[currentChar] || 1;
+    }
+  }
+
+  return obj;
+};
+
+// logs for solution 3
+console.log("Solution 3 logs: ");
+console.log(charCountUsingCharCode("aaaa"));
+/**
+ * { a: 4 }
+ */
+
+console.log(charCountUsingCharCode("hello"));
+/**
+ * { h: 1, e: 1, l: 2, o: 1 }
+ */
+
+console.log(charCountUsingCharCode("Your PIN number is 1234!"));
+/**
+ * { 1: 1, 2: 1, 3: 1, 4: 1, b: 1, e: 1, i: 2, m: 1, n: 2, o: 1, p: 1, r: 2, s: 1, u: 2, y: 1 }
+ */
+
+// -----------------[SOLUTION: 4]--------------------------- //
+const isAlphaNumericOptimised = (char) => {
+  const charCode = char.charCodeAt(0);
+  if (
+    !(charCode >= 48 && charCode <= 57) && // numeric [0-9]
+    !(charCode >= 65 && charCode <= 90) && // upper alpha [A-Z]
+    !(charCode >= 97 && charCode <= 122) // lower alpha [a-z]
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+const charCountOptimised = (str) => {
+  // return empty obj if the input string is not a string
+  if (!str) return {};
+  const result = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (!isAlphaNumericOptimised(str[i])) continue;
+    const currentChar = str[i].toLowerCase();
+    result[currentChar] = (result[currentChar] || 0) + 1;
+  }
+
+  return result;
+};
+
+// logs for solution 4
+console.log("Solution 4 logs: ");
+console.log(charCountOptimised("aaaa"));
+/**
+ * { a: 4 }
+ */
+
+console.log(charCountOptimised("hello"));
+/**
+ * { h: 1, e: 1, l: 2, o: 1 }
+ */
+
+console.log(charCountOptimised("Your PIN number is 1234!"));
 /**
  * { 1: 1, 2: 1, 3: 1, 4: 1, b: 1, e: 1, i: 2, m: 1, n: 2, o: 1, p: 1, r: 2, s: 1, u: 2, y: 1 }
  */
