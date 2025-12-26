cfa2af39d92552e1e1bd00a86d958c968fea8c66
mmkvdev <madhuv299@gmail.com>
2025-12-26 12:12:14 +0530

    code/multiple-pointers: naive solution for sum zero problem

---
+++ practise/multiple-pointers/sumZero/optimised/index.js
@@ -0,0 +1,23 @@
+// Write a function called `sumZero` which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+// Input: Sorted Array<integer>
+// Output: [-a, a] / undefined
+
+// Optimised (using two pointers)
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+const sumZero = (arr) => {
+  let left = 0,
+    right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) return [arr[left], arr[right]];
+    else if (sum > 0) right--;
+    else left++;
+  }
+};
+
+console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10])); // [-3, 3]
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
+console.log(sumZero([-2, 0, 1, 3])); // undefined
+console.log(sumZero([1, 2, 3])); // undefined
