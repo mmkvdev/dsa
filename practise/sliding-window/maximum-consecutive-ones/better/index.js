f36e3e4a2b7cd7ca7e76b245f5cb9c3d2708f99d
mmkvdev <madhuv299@gmail.com>
2026-01-24 08:41:00 +0530

    code/sliding-window: Maximum Consecutive Ones Better Solution

---
+++ practise/sliding-window/maximum-consecutive-ones/better/index.js
@@ -0,0 +1,39 @@
+// Max Consective Ones
+// Given an array which consists of 1s and 0s and an integer k, task is to return the maximum consecutive 1s given that you’re allowed to flip at most k 0s
+// Re-definition: Longest SubArray with maximum k 0s
+
+// BETTER APPROACH
+// TIME COMPLEXITY - O(n + n) => O(2n)
+// SPACE COMPLEXITY - O(1)
+
+const maxConsecutiveOnes = (arr, k) => {
+  let l = 0,
+    r = 0,
+    maxLen = 0,
+    zeroCount = 0,
+    n = arr.length;
+  while (r < n) {
+    if (arr[r] === 0) {
+      zeroCount++;
+    }
+
+    if (zeroCount <= k) {
+      maxLen = Math.max(maxLen, r - l + 1);
+    }
+
+    while (zeroCount > k) {
+      if (arr[l] === 0) {
+        zeroCount--;
+      }
+      l++;
+    }
+    r++;
+  }
+  return maxLen;
+};
+
+console.log(maxConsecutiveOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2)); // 6
+console.log(maxConsecutiveOnes([0, 0, 0, 1, 1, 1, 1], 1)); // 5
+console.log(maxConsecutiveOnes([1, 0, 0, 0, 1, 1, 1], 1)); // 4
+console.log(maxConsecutiveOnes([1, 0, 1, 0, 1, 0, 1, 0, 1], 2)); // 5
+console.log(maxConsecutiveOnes([0, 0, 0, 0], 2)); // 2
