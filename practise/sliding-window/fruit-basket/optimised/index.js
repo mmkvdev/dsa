7877b18d9811ff55fcff337c64291000e1761b8f
mmkvdev <madhuv299@gmail.com>
2026-01-26 07:31:08 +0530

    code/sliding-window: Fruit Basket Optimised Solution

--- practise/sliding-window/fruit-basket/optimised/index.js
+++ practise/sliding-window/fruit-basket/optimised/index.js
@@ -5,27 +5,40 @@
 // TIME COMPLEXITY - O(n)
 // SPACE COMPLEXITY - O(1)
 
-const totalFruit = function (fruits) {
-  let l = 0,
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function (fruits) {
+  let maxLen = 0,
+    l = 0,
     r = 0,
-    maxLen = 0,
     n = fruits.length,
-    k = 2,
-    fruitMap = new Map();
+    k = 2;
+
+  // we maintain a hash-map to store the fruits and their frequencies, this helps us keep tracking of which fruit type element to remove while shrinking the window
+  let fruitMap = new Map();
 
   while (r < n) {
+    // keep track of the fruit-type and it's frequency
     fruitMap.set(fruits[r], (fruitMap.get(fruits[r]) || 0) + 1);
 
+    // if the size of the hash-map is in control of the in-variant (k)
     if (fruitMap.size <= k) {
       maxLen = Math.max(maxLen, r - l + 1);
     }
 
+    // if the size of the hash-map exceeds the in-variant size (k)
     if (fruitMap.size > k) {
+      // decrement the frequency of the fruit-type
       fruitMap.set(fruits[l], fruitMap.get(fruits[l]) - 1);
 
+      // when the frequency of any fruit-type becomes `0`, we remove that fruit-type from the hash-map, i.e., removing that element from the window
       if (fruitMap.get(fruits[l]) === 0) {
         fruitMap.delete(fruits[l]);
       }
+
+      // advance `l` to continue the window shrinking flow
       l++;
     }
     r++;
@@ -34,4 +47,7 @@ const totalFruit = function (fruits) {
   return maxLen;
 };
 
+console.log(totalFruit([1, 2, 1])); // 3
+console.log(totalFruit([0, 1, 2, 2])); // 3
+console.log(totalFruit([1, 2, 3, 2, 2])); // 4
 console.log(totalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4])); // 5 -> [1, 2, 1, 1, 2]
