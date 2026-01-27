a6566958f2dcca0fa725fa774cbec02bc7f009f1
mmkvdev <madhuv299@gmail.com>
2026-01-27 07:24:01 +0530

    code/sliding-window: Longest Substring with at most k distinct characters Optimised Solution

---
+++ practise/sliding-window/longest-substring-with-atmost-k-distinct-characters/optimised/index.js
@@ -0,0 +1,30 @@
+// OPTIMISED APPROACH
+// TIME COMPLEXITY - O(n) => O(n)
+// SPACE COMPLEXITY - O(256)
+
+const longestSubStringWithKDistinctCharacters = (str, k) => {
+  let maxLen = 0,
+    charMap = new Map(),
+    l = 0,
+    r = 0;
+
+  while (r < str.length) {
+    charMap.set(str[r], (charMap.get(str[r]) || 0) + 1);
+    if (charMap.size <= k) {
+      maxLen = Math.max(maxLen, r - l + 1);
+    }
+
+    if (charMap.size > k) {
+      charMap.set(str[l], (charMap.get(str[l]) || 0) - 1);
+      if (charMap.get(str[l]) === 0) {
+        charMap.delete(str[l]);
+      }
+      l++;
+    }
+    r++;
+  }
+
+  return maxLen;
+};
+
+console.log(longestSubStringWithKDistinctCharacters("aaabbccd", 2)); // 5
