b5cbf1c9aa9ae3f57b0cd80904dfcd0c34bd11f9
mmkvdev <madhuv299@gmail.com>
2026-01-31 12:55:01 +0530

    code/sliding-window: Number of sub-strings containing all three characters Optimised solution

---
+++ practise/sliding-window/variable-window/number-of-substrings-containing-all-three-characters/optimised/index.js
@@ -0,0 +1,30 @@
+// Problem 17: Number of substrings containing all three characters
+// Given a string containing only a, b and c. Write a function that takes in a string and counts the number of substrings containing all three characters (a, b and c)
+// https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
+
+/**
+ * @param {string} s
+ * @return {number}
+ * TIME COMPLEXITY: O(n)
+ * SPACE COMPLEXITY: O(1)
+ */
+var numberOfSubstrings = function (s) {
+  let count = 0,
+    lastSeen = { 0: -1, 1: -1, 2: -1 },
+    n = s.length,
+    i = 0;
+
+  while (i < n) {
+    let seenKey = s[i].charCodeAt(0) - 97;
+    lastSeen[seenKey] = i;
+
+    if (lastSeen[0] !== -1 && lastSeen[1] !== -1 && lastSeen[2] !== -1) {
+      count += 1 + Math.min(...Object.values(lastSeen));
+    }
+    i++;
+  }
+
+  return count;
+};
+
+console.log(numberOfSubstrings("bbacba"));
