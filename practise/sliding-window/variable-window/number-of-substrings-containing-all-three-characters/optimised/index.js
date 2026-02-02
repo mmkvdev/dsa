bb623d453a21b02bdcb14d5add78aa36452e5ec0
mmkvdev <madhuv299@gmail.com>
2026-02-02 23:36:28 +0530

    code/sliding-window: Number of sub-strings containing all three characters Slightly Optimised solution improvements

--- practise/sliding-window/variable-window/number-of-substrings-containing-all-three-characters/optimised/index.js
+++ practise/sliding-window/variable-window/number-of-substrings-containing-all-three-characters/optimised/index.js
@@ -19,7 +19,7 @@ var numberOfSubstrings = function (s) {
     lastSeen[seenKey] = i;
 
     if (lastSeen[0] !== -1 && lastSeen[1] !== -1 && lastSeen[2] !== -1) {
-      count += 1 + Math.min(...Object.values(lastSeen));
+      count += 1 + Math.min(lastSeen[0], lastSeen[1], lastSeen[2]);
     }
     i++;
   }
