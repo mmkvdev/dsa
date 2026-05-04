e56fa4fdb1b98747b1f049f6e8b01ce074bc6d00
mmkvdev <madhuv299@gmail.com>
2026-05-04 18:09:41 +0530

    multiple pointers: Move Zeroes Optimised Implementation

---
+++ practise/multiple-pointers/moveZeroes/optimised/moveZeroes.js
@@ -0,0 +1,26 @@
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+const moveZeroes = (arr) => {
+  if (arr.length === 1) return arr;
+
+  let left = 0,
+    right = 0,
+    n = arr.length;
+
+  while (right < n) {
+    if (arr[right] !== 0) {
+      let temp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = temp;
+      left++;
+    }
+
+    right++;
+  }
+
+  return arr;
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+console.log(moveZeroes([0]));
+console.log(moveZeroes([1]));
